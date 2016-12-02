83b47a9b589ec75e6cc50768226d6a743b776ce3
Himanshu dua <hdua@q3tech.com>
2016-12-02 12:14:59 +0530

    Add command line args

--- ap.py
+++ ap.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 from bs4 import BeautifulSoup
 
@@ -12,36 +13,51 @@ def sort_dic_json(data):
     return dic_json
 
 
-with open('file.xml', 'r') as f:
-    xml_data = f.read()
-
-with open('file.json', 'r') as f:
-    json_data = json.loads(f.read())
-
-dic_json = sort_dic_json(json_data)
+def get_lst_tag(soup):
+    return list(set([tag.name for tag in soup.find_all()]))
 
 
-soup = BeautifulSoup(xml_data, 'html.parser')
-lst_tag = []
-lst_tag = list(set([tag.name for tag in soup.find_all()]))
+def execute_code(xml_file, json_file):
+    with open(xml_file, 'r') as f:
+        xml_data = f.read()
 
-correct = incorrect = 0
-for tag in lst_tag:
-    for text in soup.find_all(tag):
-        if dic_json.get(tag, None) and text.get_text().strip() in dic_json[tag]:
-            correct = correct + 1
-        else:
-            incorrect = incorrect + 1
-print('Correct count is ', correct, ' and incorrect count is ', incorrect)
+    with open(json_file, 'r') as f:
+        json_data = json.loads(f.read())
 
-new_correct = 0
-new_incorrect = 0
-for tag in dic_json:
-    for item in dic_json[tag]:
-        if soup.find_all(tag) and item.strip() in [i.get_text().strip() for i in soup.find_all(tag)]:
-            new_correct = new_correct + 1
-        else:
-            new_incorrect = new_incorrect + 1
+    dic_json = sort_dic_json(json_data)
 
-print('New Correct count is ', new_correct,
-      ' and New incorrect count is ', new_incorrect)
+    soup = BeautifulSoup(xml_data, 'html.parser')
+    lst_tag = get_lst_tag(soup)
+
+    correct, incorrect = 0, 0
+    for tag in lst_tag:
+        for text in soup.find_all(tag):
+            if dic_json.get(tag, None) and text.get_text().strip() in dic_json[tag]:
+                correct = correct + 1
+            else:
+                incorrect = incorrect + 1
+    print('Correct count is ', correct, ' and incorrect count is ', incorrect)
+
+    new_correct = 0
+    new_incorrect = 0
+    for tag in dic_json:
+        for item in dic_json[tag]:
+            if soup.find_all(tag) and item.strip() in [i.get_text().strip() for i in soup.find_all(tag)]:
+                new_correct = new_correct + 1
+            else:
+                new_incorrect = new_incorrect + 1
+
+    print('New Correct count is ', new_correct,
+          ' and New incorrect count is ', new_incorrect)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('xml_file', help='xml file')
+    parser.add_argument('json_file', help='json file')
+    args = parser.parse_args()
+    execute_code(args.xml_file, args.json_file)
+
+
+if __name__ == '__main__':
+    main()
